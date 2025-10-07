e6bf0cc5776d57c1164343a9665566c6c5956b9c
JezzDiego <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>
2025-10-07 11:08:17 -0300

    first commit

---
+++ src/apps/web/app.tsx
@@ -0,0 +1,12 @@
+import { Spinner } from "./components/ui/spinner";
+
+function App() {
+  return (
+    <div>
+      <img src="/assets/vite.svg" alt="Vite logo" />
+      <Spinner />
+    </div>
+  );
+}
+
+export default App;
